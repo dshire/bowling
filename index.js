68c172012ca5fb10b65102f33228a187906a9c85
dshire <wellereitetclub@gmail.com>
2017-10-26 16:44:32 +0200

    added rolling mechanism, next step: displaying

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ app.get('/',function(req,res){
 });
 
 io.on('connection', function(socket) {
+    let playerFrames = [];
+    let frame = 0;
 
     socket.emit('welcome');
+
+    socket.on('roll', () => {
+
+        if (!playerFrames[frame]) {
+            playerFrames.push({})
+        }
+
+        if ( !playerFrames[frame].roll1 && playerFrames[frame].roll1 !== 0) {
+            let result = Math.floor(Math.random() * 11);
+            playerFrames[frame].roll1 = result;
+
+            if (result == 10) {
+                playerFrames[frame].roll2 = 0;
+                frame++;
+            }
+
+        } else {
+            let result = Math.floor(Math.random() * (11 - playerFrames[frame].roll1));
+            playerFrames[frame].roll2 = result;
+
+            frame++;
+
+        }
+        console.log(playerFrames);
+
+        socket.emit('roll', {playerFrames});        
+    });
 });
 
 server.listen(8080, function() {
--- public/script.js
+++ public/script.js
@@ -3,3 +3,13 @@ const socket = io.connect();
 socket.on('welcome',() => {
     document.body.insertAdjacentHTML('beforeend', '<div class="welcome">Welcome to Babbelonian Bowling</div>');
 });
+
+
+$('.button').on('click',() => {
+    socket.emit('roll');
+});
+
+socket.on('roll', (data) => {
+    console.log(data.playerFrames);
+
+});
