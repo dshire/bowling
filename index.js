45e36fc0896bc563e1403d926167d7b3e970e9f8
dshire <wellereitetclub@gmail.com>
2017-10-26 17:17:44 +0200

    added score calculations, display of points and score

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.get('/',function(req,res){
 io.on('connection', function(socket) {
     let playerFrames = [];
     let frame = 0;
-
     socket.emit('welcome');
 
     socket.on('roll', () => {
@@ -29,18 +28,50 @@ io.on('connection', function(socket) {
                 playerFrames[frame].roll2 = 0;
                 frame++;
             }
-
         } else {
             let result = Math.floor(Math.random() * (11 - playerFrames[frame].roll1));
             playerFrames[frame].roll2 = result;
 
             frame++;
-
         }
+
+        calcScore()
+
         console.log(playerFrames);
 
-        socket.emit('roll', {playerFrames});        
+        socket.emit('roll', {playerFrames});
     });
+
+    function calcScore() {
+        let score = 0;
+        for (let i = 0; i < playerFrames.length; i++) {
+            if (playerFrames[i].roll1 >= 0 && playerFrames[i].roll2 >= 0) {
+
+                if (playerFrames[i].roll1 == 10 || playerFrames[i].roll2 == 10){
+                    if (playerFrames[i+1] && playerFrames[i+1].roll1) {
+                        if (playerFrames[i+1].roll1 == 10) {
+                            if (playerFrames[i+2] && playerFrames[i+2].roll1) {
+                                score += playerFrames[i+2].roll1;
+                            }
+                        } else {
+                            score += playerFrames[i+1].roll1;
+                            if (playerFrames[i+1] && playerFrames[i+1].roll2) {
+                                score += playerFrames[i+1].roll2;
+                            }
+                        }
+                    }
+                } else if (playerFrames[i].roll1 + playerFrames[i].roll2 == 10) {
+                    if (playerFrames[i+1] && playerFrames[i+1].roll1) {
+                        score += playerFrames[i+1].roll1;
+                    }
+                }
+
+                score += (playerFrames[i].roll1 + playerFrames[i].roll2)
+                playerFrames[i].score = score
+
+            }
+        }
+    }
 });
 
 server.listen(8080, function() {
--- public/script.js
+++ public/script.js
@@ -12,4 +12,12 @@ $('.button').on('click',() => {
 socket.on('roll', (data) => {
     console.log(data.playerFrames);
 
+    for (let i = 0; i < data.playerFrames.length; i++) {
+        var frame="frame" + i;
+        if (data.playerFrames[i].roll2 >= 0) {
+            $('.' + frame).html(`<p class="roll1">${data.playerFrames[i].roll1}</p><p class="roll2">${data.playerFrames[i].roll2}</p><p class="score">${data.playerFrames[i].score}</p>`)
+        } else {
+            $('.' + frame).html(`<span class="points"><p class="roll1">${data.playerFrames[i].roll1}</p></span>`)
+        }
+    }
 });
